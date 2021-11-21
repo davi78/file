6cf2658b00f78118b18e0c0e860dbc56ce774bb8
davi78 <78717503+davi78@users.noreply.github.com>
2021-11-22 02:04:46 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -11,6 +11,9 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
+#username
+USERNAME = os.environ.get("USERNAME", "")
+
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
